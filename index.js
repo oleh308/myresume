0a53f28c9718c118776896585a487347cc09a21a
Oleh Busko <busko.oleh@gmail.com>
2020-07-06 16:45:47 +0100

    removed import

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-var wkhtmltox = require("wkhtmltox");
+// var wkhtmltox = require("wkhtmltox");
 
 // instantiate a new converter.
-var converter = new wkhtmltox();
+// var converter = new wkhtmltox();
 
 app.get('/resume', (req, res) => {
-  converter.pdf(fs.createReadStream('src/index.html'), { })
-      .pipe(res)
-      .on("finish", () => {
-        console.log('done')
-      });
+  // converter.pdf(fs.createReadStream('src/index.html'), { })
+  //     .pipe(res)
+  //     .on("finish", () => {
+  //       console.log('done')
+  //     });
+
+  res.send('something')
 });
 
 const server_host = '0.0.0.0';
 
-app.listen(PORT, server_host,  () => {
+app.listen(PORT,  () => {
   console.log('Listening to port:', process.env.PORT || PORT);
 });
