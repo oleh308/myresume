c821124c3e998ada964b49699eda102660086b0d
Oleh Busko <busko.oleh@gmail.com>
2020-07-06 16:49:27 +0100

    Port fix v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
