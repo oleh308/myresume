0b5b03597889142a19feee2e49147b8bdb308191
Oleh Busko <busko.oleh@gmail.com>
2020-07-06 23:21:37 +0100

    Minor fixes and SVG source change

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const express = require('express');
 var wkhtmltopdf = require('wkhtmltopdf');
 const { baseUrl } = require('./src/utils/url');
 
-
 if (process.env.NODE_ENV === 'production') {
   wkhtmltopdf.shell = '/bin/ash';
-  console.log('here');
 }
 
 const PORT = process.env.PORT || 3000
@@ -20,27 +18,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-// app.get('/resume/preview', (req, res) => {
-//   console.log('here')
-//   let $ = cheerio.load(fs.readFileSync('src/index.html'));
-//
-//   let html = $.html();
-//   html = html.replace('css/style.css', baseUrl(req) + '/css/style.css')
-//   console.log('here')
-//
-//   wkhtmltopdf(html, { marginTop: '2mm', marginLeft: '8mm', marginRight: '8mm' })
-//     .pipe(res);
-// });
-
 app.get('/resume', async (req, res) => {
   let $ = cheerio.load(fs.readFileSync('src/index.html'));
   $('body').addClass("whiteBackground");
   let button = $('#downloadButton');
-
+  button.remove();
   let html = $.html();
-  html = html.replace('css/style.css', baseUrl(req) + '/css/style.css')
-  res.setHeader('Content-disposition', 'attachment; filename=OBuskoResume.pdf');
+  html = html.replace('css/style.css', baseUrl(req) + '/css/style.css');
+  html = html.replace(/svgs/g, baseUrl(req) + '/svgs');
 
+  const options = {
+    marginTop: '2mm',
+    marginLeft: '8mm',
+    marginRight: '8mm',
+    javascriptDelay: 500
+  }
+
+  res.setHeader('Content-disposition', 'attachment; filename=OBuskoResume.pdf');
   wkhtmltopdf(html, { marginTop: '2mm', marginLeft: '8mm', marginRight: '8mm' })
     .pipe(res);
 });
